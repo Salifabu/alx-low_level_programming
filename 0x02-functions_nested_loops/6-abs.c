a186507cc62fb899fdd846a68d532b11afaf6b8c
Salifabu <otuemobeabubakar@gmail.com>
2023-09-13 16:00:57 -0700

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+	int n
+
+	n = _abs('-');
+{
+	if (n < 0)
+	{
+	int abs_val;
+
+	abs_val = n * -1;
+	return (abs_val);
+	}
+	return (n);
+}
