8c8d4199b23ec18b4c06b0c3988248026345e51a
Salifabu <otuemobeabubakar@gmail.com>
2023-09-24 13:19:11 -0700

    Write a function that draws a diagonal line on the terminal

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-char c;
-	
-for (c = '0'; c <= '9'; c++)
+	int c;
+
+	for (c = 48; c < 58; c++)
 {
-_putchar(c);
+	_putchar(c);
 }
-_putchar('\n');
+	_putchar('\n');
 }
