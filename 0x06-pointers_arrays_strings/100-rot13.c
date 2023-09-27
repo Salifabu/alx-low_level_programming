665086f8486aa8e1b1b60e4feee1d088c994a22f
Salifabu <otuemobeabubakar@gmail.com>
2023-09-27 13:29:57 -0700

    a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rott13 - a function that encode a string using rot13
+ * rot13 - function that encodes a string using rot13
  * @str: set of strings to be encoded
  * Return: string out
  */
@@ -12,17 +13,17 @@ char *rot13(char *str)
 	char c[] = "ABUCDEGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char d[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-		for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 {
-		for (j = 0; c[j] != '\0'; j++)
+	for (j = 0; c[j] != '\0'; j++)
 {
-			if (str[i] == c[j])
+	if (str[i] == c[j])
 {
-				str[i] = d[j];
-				break;
+	str[i] = d[j];
+	break;
 }
 
 }
 }
-			return (str);
+	return (str);
 }
