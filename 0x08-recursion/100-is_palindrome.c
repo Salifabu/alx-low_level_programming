d316d353a5fd993a86e040c1d40f1c17f6a9b91d
Salifabu <otuemobeabubakar@gmail.com>
2023-10-01 12:35:45 -0700

    a function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,51 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * prime_a - to calculate if its a prime number
- * @i: input number
- * @j: divisor
- * Return: (o)
+ * _strlen_recursion - to get the length of string
+ * @s: string
+ *
+ * Return: length of the string
  */
 
-int prime_a(int i, int j)
+int _strlen_recursion(char *s)
 {
-        if (i <= 1 || (i != j && i % j == 0))
-{
-        return (0);
-}
-        else if (i == j)
-{
-        return (1);
-}
-        return (prime_a(i, j + 1));
+if (*s == '\0')
+	return (0);
+
+	else
+
+	return (1 + _strlen_recursion(s + 1));
+
 }
 /**
- * is_prime_number - detect if its a prime number
- * @n: the input number
- * Return: 0 or 1
+ * compare_string - compare each character of the string
+ * @s: string
+ * @left: smallest iterator
+ * @right: largest iterator
+ * Return: int
  */
 
-int is_prime_number(int n)
+int compare_string(char *s, int left, int right)
 {
-        return (prime_a(n, 2));
+	if (*(s + left) == *(s + right))
+{
+	if (left == right || left == right + 1)
+	return (1);
+	return (0 + compare_string(s, left + 1, right - 1));
+}
+	return (0);
+
+}
+/**
+ * is_palindrome - detects if a string is a palindrome
+ * @s: the string to test
+ * Return: 1 if it is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	return (1);
+	return (compare_string(s, 0, _strlen_recursion(s - 1)));
+
 }
