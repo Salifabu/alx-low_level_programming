bd5469590e1b05cb07855d3a96a797ae51e58097
Salifabu <alminad24@gmail.com>
2023-11-23 19:48:35 -0800

    a function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: string containing a binary number
+ *
+ * Return: unsigned int value of binsry number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int x;
+	unsigned int number;
+
+	number = 0;
+	if (!b)
+		return (0);
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (0);
+}
+	for (x = 0; b[x] != '\0'; x++)
+{
+		number <<= 1;
+		if (b[x] == '1')
+			number += 1;
+}
+	return (number);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+[200~0x14. C - Bit manipulation
+Readme.md for Alx projects Bit manipulation
