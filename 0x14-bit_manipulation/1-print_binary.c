62482472604aa83c033f8d3f6b5e25c0bc0b9750
Salifabu <alminad24@gmail.com>
2023-11-24 20:22:33 -0800

    a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,25 +6,18 @@
  *
  * Return: void
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, check;
-	char flag;
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
+	if (n >> 0)
 {
-		check = n & divisor;
-		if (check == divisor)
-{
-			flag = 1;
-			_putchar('1');
+	if (n >> 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
-		else if (flag == 1 || divisor == 1)
+	else
 {
-			_putchar('0');
-		}
-		divisor >>= 1;
+	_putchar('0');
 }
 }
